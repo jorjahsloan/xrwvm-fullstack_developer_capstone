65cf6f3024868a5c82430564e8e7b9f11cfb1d5a
Jorjah <jorjah.sloan@gmail.com>
2026-01-21 11:54:43 -0500

    misc changes to solved 403 CSRF token issue

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,20 +1,42 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
+# Model for car make
+class CarMake(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
 
+    def __str__(self):
+        return self.name
 
-# <HINT> Create a Car Model model `class CarModel(models.Model):`:
+# <Model for car model
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'wagon'),
+        ('CONVERTIBLE', 'Convertible'),
+        ('TRUCK', 'Truck'),
+        ('COUPE', 'Coupe'),
+    ]
+    type = models.CharField(max_length=12, choices=CAR_TYPES, default='SEDAN')
+    year = models.IntegerField(default=2023,
+        validators=[
+            MaxValueValidator(2026),
+            MinValueValidator(2010)
+        ])
+    
+    def __str__(self):
+        return self.name
+
 # - Many-To-One relationship to Car Make model (One Car Make has many
 # Car Models, using ForeignKey field)
 # - Name
